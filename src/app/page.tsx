97f84d0bc068ab97eb124068e519f54e6370a9fc
1harios <155941723+1harios@users.noreply.github.com>
2026-09-01 19:38:29 +0300

    Remove secondary sections from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import { BuildStepsBlock } from '@/components/home/BuildStepsBlock'
-import { CabinetBlock } from '@/components/home/CabinetBlock'
 import { FaqBlock } from '@/components/home/FaqBlock'
 import { FinalCta } from '@/components/home/FinalCta'
 import { FeaturedHomes } from '@/components/home/FeaturedHomes'
 import { Hero } from '@/components/home/Hero'
 import { WhyUsBlock } from '@/components/home/WhyUsBlock'
-import { ObjectsSlider } from '@/components/home/ObjectsSlider'
 import { ProjectsPreview } from '@/components/home/ProjectsPreview'
-import { ReviewsBlock } from '@/components/home/ReviewsBlock'
-import { TeamBlock } from '@/components/home/TeamBlock'
 import { TechnologyBlock } from '@/components/home/TechnologyBlock'
 import { company } from '@/content/company'
 import { faqItems } from '@/content/faq'
@@ -68,11 +63,6 @@ export default async function HomePage() {
       <WhyUsBlock />
       <TechnologyBlock />
       <ProjectsPreview projects={projects} />
-      <BuildStepsBlock />
-      <CabinetBlock />
-      <ObjectsSlider />
-      <ReviewsBlock />
-      <TeamBlock />
       <FaqBlock />
       <FinalCta />
     </>
